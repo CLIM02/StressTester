149d7708f65a40dbd3e3a98de7bb316e6b048e71
tt <tangtaoit@githubim.com>
2025-05-22 11:58:20 +0800

    update

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"flag"
-	"log"
-	"os"
-	"syscall"
 
 	"github.com/WuKongIM/StressTester/server"
 )
@@ -24,13 +21,13 @@ func main() {
 
 	var err error
 
-	logFile, err := os.OpenFile("./fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
-	if err != nil {
-		log.Println("服务启动出错", "打开异常日志文件失败", err)
-		return
-	}
-	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
-	syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
+	// logFile, err := os.OpenFile("./fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
+	// if err != nil {
+	// 	log.Println("服务启动出错", "打开异常日志文件失败", err)
+	// 	return
+	// }
+	// // 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
+	// syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
 
 	s := server.New(server.NewOptions(server.WithAddr(":9466"), server.WithServer(*serverAddr), server.WithNewSeq(*seq)))
 	err = s.Run()
